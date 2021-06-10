3ce2767df4eb9843aa1f93b881a48aceafd7b978
Илья Костюков <wtswrngwm>
2021-06-10 14:03:46 +0300

    style(TEST-4): changed class names, add comments to files

--- TEST-4/js/script.js
+++ TEST-4/js/script.js
@@ -1,28 +1,28 @@
-const containerOne = document.getElementById("first-container");
+const containerFirst = document.getElementById("first-container");
 const shareBtn = document.getElementById("share-button");
 const learnMoreBtn = document.getElementById("learnmore-button");
-const containerTwo = document.getElementById("second-container");
+const containerSecond = document.getElementById("second-container");
 const freeSampleBtn = document.getElementById("freesample-button");
 const reviewBtn = document.getElementById("review-button");
-const containerThree = document.getElementById("third-container");
+const containerThird = document.getElementById("third-container");
 const noBtn = document.getElementById("no-button");
 const yesBtn = document.getElementById("yes-button");
 
 shareBtn.addEventListener('click', function() {
-    containerOne.classList.add('invisible-container')
+    containerFirst.classList.add('invisible-container')
 })
 learnMoreBtn.addEventListener('click', function() {
-    containerOne.classList.add('invisible-container')
+    containerFirst.classList.add('invisible-container')
 })
 freeSampleBtn.addEventListener('click', function() {
-    containerTwo.classList.add('invisible-container')
+    containerSecond.classList.add('invisible-container')
 })
 reviewBtn.addEventListener('click', function() {
-    containerTwo.classList.add('invisible-container')
+    containerSecond.classList.add('invisible-container')
 })
 noBtn.addEventListener('click', function() {
-    containerThree.classList.add('invisible-container')
+    containerThird.classList.add('invisible-container')
 })
 yesBtn.addEventListener('click', function() {
-    containerThree.classList.add('invisible-container')
+    containerThird.classList.add('invisible-container')
 })
